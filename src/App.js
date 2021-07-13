d26af321fec1d3a8a88d1bd64248c3d13a0ed212
lucasBRYG <adio.lucas@gmail.com>
2021-07-12 21:45:53 -0400

    created checkout and subtotal component(currency-format package)

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import React from "react";
-
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Header from "./header/Header";
 import Home from "./home/Home";
+import Checkout from "./checkout/Checkout";
 
 import './App.css';
 function App() {
   return (
-    <div className="App">
-      <Header/>
-      <Home/>
-    </div>
+    <Router>
+      <div className="App">
+        <Header/>
+
+        <Switch>
+          <Route path = "/checkout">
+            <Checkout/>
+          </Route>
+          <Route path = "/">
+            <Home/>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+    
   );
 }
 
--- src/header/Header.js
+++ src/header/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 //importing icons from material-ui
 import SearchIcon from '@material-ui/icons/Search';
@@ -8,7 +9,10 @@ import "./Header.css";
 function Header() {
     return (
         <div className = "header">
-            <img src = "http://pngimg.com/uploads/amazon/amazon_PNG25.png" className = "header__logo"/>
+            <Link to = "/">
+                <img src = "http://pngimg.com/uploads/amazon/amazon_PNG25.png" className = "header__logo"/>
+            </Link>
+            
             <div className = "header__search">
                 <input className = "header__searchInput" type = "text"/>
                 <SearchIcon className = "header__searchIcon"/>
@@ -38,11 +42,13 @@ function Header() {
                         Prime
                     </span>
                 </div>
-                <div className = "header__optionBasket">
-                    <ShoppingCartIcon/>
-                    <span className = "header__optionLineTwo header__basketCount">0</span>
-                </div>
-
+                <Link to = "/checkout">
+                    <div className = "header__optionBasket">
+                        <ShoppingCartIcon/>
+                        <span className = "header__optionLineTwo header__basketCount">0</span>
+                    </div>
+                </Link>
+                
             </div>
         </div>
     )
