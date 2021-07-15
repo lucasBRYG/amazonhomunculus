51d5b7f7da6f18ff41a9d62eab7b55a516dd3ea2
lucasBRYG <adio.lucas@gmail.com>
2021-07-14 21:32:36 -0400

    Fixing bug on sheckout page when user is null

--- src/checkout/Checkout.js
+++ src/checkout/Checkout.js
@@ -16,7 +16,7 @@ function Checkout() {
                 <img className = "checkout__ad" src = "https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" alt = "banner ad for checkout screen."/>
 
                 <div>
-                    <h3>{user.email}</h3>
+                    <h3>Hello {user ? user.email : `Guest`}</h3>
                     <h2 className = "checkout__title">Your Shopping Basket</h2>
 
                     {basket.map(item => (
